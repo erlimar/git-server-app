7eaf1261bb3853dd2a2c6780ad240abee29fdf25
Erlimar Silva Campos <erlimar.campos@squadra.com.br>
2019-06-27 14:44:53 -0300

    Implementa chamada ao executável git

--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -26,12 +28,77 @@ namespace app
 
         private Task GitHandle(IDictionary<string, object> environment)
         {
+            // Request data
             var requestHeaders = (IDictionary<string, string[]>)environment["owin.RequestHeaders"];
+            var requestMethod = (string)environment["owin.RequestMethod"];
+            var requestPath = (string)environment["owin.RequestPath"];
+            var requestQueryString = (string)environment["owin.RequestQueryString"];
+            var requestBody = (Stream)environment["owin.RequestBody"];
+
+            // Response data
+            var responseBody = (Stream)environment["owin.ResponseBody"];
             var responseHeaders = (IDictionary<string, string[]>)environment["owin.ResponseHeaders"];
 
             // git clone
             // > GET /repository.git/info/refs?service=git-upload-pack HTTP/1.1
+            if (requestMethod == "GET" &&
+                requestPath.EndsWith("/info/refs") &&
+                requestQueryString == "service=git-upload-pack")
+            {
+                string processOutput = null;
+
+                // git receive-pack --stateless-rpc --advertise-refs {repo-dir}
+                using (var process = new Process())
+                {
+                    process.StartInfo.FileName = @"C:\Program Files\Git\cmd\git.exe";
+                    process.StartInfo.Arguments = string.Format("receive-pack --stateless-rpc --advertise-refs \"{0}\"", @"C:\Users\Erlimar\source\git-server-tests\server\repository");
+                    process.StartInfo.UseShellExecute = false;
+                    process.StartInfo.RedirectStandardOutput = true;
+
+                    process.Start();
+
+                    processOutput = process.StandardOutput.ReadToEnd();
+
+                    process.WaitForExit();
+                }
+
+                if (string.IsNullOrEmpty(processOutput))
+                    return responseBody.FlushAsync();
+
+                byte[] output = Encoding.UTF8.GetBytes(processOutput);
+
+                // header
+                //var pack = "#service=git-upload-pack\n";
+                //var pack = " service=git-upload-pack\n";
+                var pack = " service=git-upload-pack\n";
+                var prefix = (pack.Length + 4).ToString("x4");
+                var header = $"{prefix}{pack}0000\n";
+                var headerBytes = Encoding.UTF8.GetBytes(header);
+
+                responseHeaders["Expires"] = new string[] { "Fri, 01 Jan 1980 00:00:00 GMT" };
+                responseHeaders["Pragma"] = new string[] { "no-cache" };
+                responseHeaders["Cache-Control"] = new string[] { "no-cache, max-age=0, must-revalidate" };
+                responseHeaders["Content-Length"] = new string[] { (output.Length + header.Length).ToString() };
+                responseHeaders["Content-Type"] = new string[] { "application/x-git-receive-pack-advertisement" };
+
+                environment["owin.ResponseStatusCode"] = 200;
+                environment["owin.ResponseReasonPhrase"] = "OK";
+
+                responseBody.Write(headerBytes, 0, headerBytes.Length);
+
+                return responseBody.WriteAsync(output, 0, output.Length);
+            }
+
             // > GET /repository.git/HEAD HTTP/1.1
+            if (requestMethod == "GET" &&
+                requestPath.EndsWith("/HEAD") &&
+                string.IsNullOrEmpty(requestQueryString))
+            {
+                environment["owin.ResponseStatusCode"] = 200;
+                environment["owin.ResponseReasonPhrase"] = "OK";
+
+                return responseBody.FlushAsync();
+            }
 
             throw new UnauthorizedAccessException();
 
